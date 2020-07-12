b940830106e3998a87c70abef414b2ec2157edf8
Mohammed Asslahi <asslahi37@gmail.com>
2020-07-12 12:57:53 +0300

    added test data

---
+++ script.py
@@ -0,0 +1,3 @@
+destinations = ["Paris, France", "Shanghai, China","Los Angeles, USA","Sao Paulo, Brazil","Cairo, Egypt"]
+
+test_traveler = ["Erin Wilkes", "Shanghai, China","Historical site","art"]
