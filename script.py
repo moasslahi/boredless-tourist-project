db42781e0560833e202a090c8cc2c9cc3132eecc
Mohammed Asslahi <asslahi37@gmail.com>
2020-07-13 00:34:46 +0300

    test traveler code

--- script.py
+++ script.py
@@ -1,3 +1,33 @@
-destinations = ["Paris, France", "Shanghai, China","Los Angeles, USA","Sao Paulo, Brazil","Cairo, Egypt"]
+destinations = ["Paris, France","Shanghai, China","Los Angeles, USA","Sao Paulo, brazil","Cairo, Egypt"]
 
-test_traveler = ["Erin Wilkes", "Shanghai, China","Historical site","art"]
+test_traveler = ["Erin", "Shanghai, China",["Historical site","art" ]]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+print(get_destination_index("Los Angeles, USA"))
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  travelter_destination_index = get_destination_index(traveler_destination)
+  return travelter_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
